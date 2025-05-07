089ac79b8a3af38e695f7c1ca6af7ded886c153c
Rauneet Singh <rauneet.03@outlook.com>
2025-05-07 16:21:18 +0530

    revert

--- HarshitCommunications/Program.cs
+++ HarshitCommunications/Program.cs
@@ -36,6 +36,7 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 
+builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(100);
